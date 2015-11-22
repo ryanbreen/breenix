c6d6b965afc203feb922b77284269579d5378351
Ryan Breen <rbreen@cimpress.com>
2015-11-22 15:35:00 -0500

    Fix nasm warning.

--- multiboot_header.asm
+++ multiboot_header.asm
@@ -4,7 +4,7 @@ header_start:
     dd 0                         ; architecture 0 (protected mode i386)
     dd header_end - header_start ; header length
     ; checksum
-    dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
+    dd (0xe85250d6 + 0 + (header_end - header_start))
 
     ; insert optional multiboot tags here
 
