a2a63242686770eb36eba9d6adeedabe3c77fd5f
Ryan Breen <rbreen@cimpress.com>
2015-11-28 17:51:02 -0500

    Simple output from rust complete.

--- src/arch/x86_64/long_mode_init.asm
+++ src/arch/x86_64/long_mode_init.asm
@@ -8,10 +8,15 @@ long_mode_start:
     call setup_SSE
     call rust_main
 
-    ; print `OKAY` to screen
-    mov rax, 0x2f592f412f4b2f4f
-    mov qword [0xb8000], rax
-    hlt
+    .os_returned:
+        ; rust main returned, print `OS returned!`
+        mov rax, 0x4f724f204f534f4f
+        mov [0xb8000], rax
+        mov rax, 0x4f724f754f744f65
+        mov [0xb8008], rax
+        mov rax, 0x4f214f644f654f6e
+        mov [0xb8010], rax
+        hlt
 
 ; Prints `ERROR: ` and the given error code to screen and hangs.
 ; parameter: error code (in ascii) in al
--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,19 @@ extern crate rlibc;
 
 #[no_mangle]
 pub extern fn rust_main() {
-  let test = (0..3).flat_map(|x| 0..x).zip(0..);
-  let x = ["Hello", " ", "World", "!"];
-  let mut a = 42;
-  a += 1;
+  let hello = b"Hello World!";
+  let color_byte = 0x1f; // white foreground, blue background
+
+  let mut hello_colored = [color_byte; 24];
+  for (i, char_byte) in hello.into_iter().enumerate() {
+    hello_colored[i*2] = *char_byte;
+  }
+
+  // write `Hello World!` to the center of the VGA text buffer
+  let buffer_ptr = (0xb8000 + 1988) as *mut _;
+  unsafe { *buffer_ptr = hello_colored };
+
+  loop{}
 }
 
 #[lang = "eh_personality"] extern fn eh_personality() {}
