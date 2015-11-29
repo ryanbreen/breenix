265350875556a14b9dd16665dd70cabcc1daecd9
Ryan Breen <rbreen@cimpress.com>
2015-11-28 20:06:34 -0500

    Simple vga buffer.

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,13 @@
-#![feature(no_std, lang_items)]
+#![feature(no_std, lang_items, const_fn, unique, core_str_ext)]
 #![no_std]
 
 extern crate rlibc;
 
+mod vga_buffer;
+
 #[no_mangle]
-pub extern fn rust_main() {
-  let hello = b"Hello World!";
-  let color_byte = 0x1f; // white foreground, blue background
-
-  let mut hello_colored = [color_byte; 24];
-  for (i, char_byte) in hello.into_iter().enumerate() {
-    hello_colored[i*2] = *char_byte;
-  }
-
-  // write `Hello World!` to the center of the VGA text buffer
-  let buffer_ptr = (0xb8000 + 1988) as *mut _;
-  unsafe { *buffer_ptr = hello_colored };
+pub unsafe extern fn rust_main() {
+  vga_buffer::print_something();
 
   loop{}
 }
---
+++ src/vga_buffer.rs
@@ -0,0 +1,94 @@
+use core::ptr::Unique;
+use core::fmt::Write;
+
+#[repr(u8)]
+pub enum Color {
+    Black      = 0,
+    Blue       = 1,
+    Green      = 2,
+    Cyan       = 3,
+    Red        = 4,
+    Magenta    = 5,
+    Brown      = 6,
+    LightGray  = 7,
+    DarkGray   = 8,
+    LightBlue  = 9,
+    LightGreen = 10,
+    LightCyan  = 11,
+    LightRed   = 12,
+    Pink       = 13,
+    Yellow     = 14,
+    White      = 15,
+}
+
+#[derive(Clone, Copy)]
+struct ColorCode(u8);
+
+impl ColorCode {
+    const fn new(foreground: Color, background: Color) -> ColorCode {
+        ColorCode((background as u8) << 4 | (foreground as u8))
+    }
+}
+
+#[repr(C)]
+struct ScreenChar {
+    ascii_character: u8,
+    color_code: ColorCode,
+}
+
+const BUFFER_HEIGHT: usize = 25;
+const BUFFER_WIDTH: usize = 80;
+
+struct Buffer {
+    chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT],
+}
+
+pub struct Writer {
+    column_position: usize,
+    color_code: ColorCode,
+    buffer: Unique<Buffer>,
+}
+
+impl Writer {
+  pub fn write_byte(&mut self, byte: u8) {
+    match byte {
+      b'\n' => self.new_line(),
+      byte => {
+        if self.column_position >= BUFFER_WIDTH {
+            self.new_line();
+        }
+
+        let row = BUFFER_HEIGHT - 1;
+        let col = self.column_position;
+
+        self.buffer().chars[row][col] = ScreenChar {
+            ascii_character: byte,
+            color_code: self.color_code,
+        };
+        self.column_position += 1;
+      }
+    }
+  }
+
+  pub fn write_str(&mut self, s: &str) {
+    for byte in s.bytes() {
+      self.write_byte(byte)
+    }
+  }
+
+  fn buffer(&mut self) -> &mut Buffer {
+    unsafe{ self.buffer.get_mut() }
+  }
+
+  fn new_line(&mut self) {/* TODO */}
+}
+
+pub unsafe fn print_something() {
+  let mut writer = Writer {
+      column_position: 0,
+      color_code: ColorCode::new(Color::LightGreen, Color::Black),
+      buffer: Unique::new(0xb8000 as *mut _),
+  };
+
+  writer.write_str("Happy dance!");
+}
