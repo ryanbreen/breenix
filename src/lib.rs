e23a82226e225027b58f81cf3522d017976cec0c
Ryan Breen <rbreen@cimpress.com>
2016-01-10 02:09:20 -0500

    Working kernel remap.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![feature(no_std, lang_items, const_fn, unique, core_slice_ext, core_str_ext, iter_cmp, asm)]
+#![feature(no_std, lang_items, const_fn, unique, core_slice_ext, core_str_ext, iter_cmp, asm, step_by)]
 #![no_std]
 
 extern crate rlibc;
@@ -24,67 +24,42 @@ extern fn panic_fmt(fmt: core::fmt::Arguments, file: &str, line: u32) -> ! {
 }
 
 #[no_mangle]
-pub extern fn rust_main(multiboot_information_address: usize) {
-  vga_buffer::clear_screen();
-/*
-  println!("Hello World{}", "!");
-  println!("Whoop these numbers {} {}", 42, 100/33);
-*/
-  let boot_info = unsafe{ multiboot2::load(multiboot_information_address) };
-  let memory_map_tag = boot_info.memory_map_tag().expect("Memory map tag required");
-/*
-  println!("memory areas:");
-  for area in memory_map_tag.memory_areas() {
-    println!("    start: 0x{:x}, length: 0x{:x}", area.base_addr, area.length);
-  }
-*/
-  let elf_sections_tag = boot_info.elf_sections_tag().expect("Elf-sections tag required");
-/*
-  println!("kernel sections:");
-  for section in elf_sections_tag.sections() {
-    println!("    addr: 0x{:x}, size: 0x{:x}, flags: 0x{:x}", section.addr, section.size, section.flags);
-  }
-*/
-  let kernel_start = elf_sections_tag.sections().map(|s| s.addr).min().unwrap();
-  let kernel_end = elf_sections_tag.sections().map(|s| s.addr + s.size).max().unwrap();
-  println!("kernel start={} kernel end={}", kernel_start, kernel_end);
-  let multiboot_start = multiboot_information_address;
-  let multiboot_end = multiboot_start + (boot_info.total_size as usize);
-  println!("multiboot start={} multiboot end={}", multiboot_start, multiboot_end);
+pub extern "C" fn rust_main(multiboot_information_address: usize) {
+    // ATTENTION: we have a very small stack and no guard page
 
-  let mut frame_allocator = memory::AreaFrameAllocator::new(
-    kernel_start as usize, kernel_end as usize, multiboot_start,
-    multiboot_end, memory_map_tag.memory_areas());
-/*
-  for i in 0.. {
-    use memory::FrameAllocator;
-    if let None = frame_allocator.allocate_frame() {
-      println!("allocated {} frames", i);
-      break;
-    }
-  }
-*/
-  memory::test_paging(&mut frame_allocator);
+    // the same as before
+    vga_buffer::clear_screen();
+    println!("Hello World{}", "!");
 
+    let boot_info = unsafe { multiboot2::load(multiboot_information_address) };
+    let memory_map_tag = boot_info.memory_map_tag()
+        .expect("Memory map tag required");
+    let elf_sections_tag = boot_info.elf_sections_tag()
+        .expect("Elf sections tag required");
+
+    let kernel_start = elf_sections_tag.sections().map(|s| s.addr).min().unwrap();
+    let kernel_end = elf_sections_tag.sections().map(|s| s.addr + s.size).max()
+        .unwrap();
+
+    let multiboot_start = multiboot_information_address;
+    let multiboot_end = multiboot_start + (boot_info.total_size as usize);
+
+    println!("kernel start: 0x{:x}, kernel end: 0x{:x}",
+        kernel_start, kernel_end);
+    println!("multiboot start: 0x{:x}, multiboot end: 0x{:x}",
+        multiboot_start, multiboot_end);
+
+    let mut frame_allocator = memory::AreaFrameAllocator::new(
+        kernel_start as usize, kernel_end as usize, multiboot_start,
+        multiboot_end, memory_map_tag.memory_areas());
+
+    // this is the new part
+    memory::remap_the_kernel(&mut frame_allocator, boot_info);
+    println!("It did not crash!");
+
+    loop {}
 }
 
-/**
-  loop {
-    unsafe {
-      let scancode = io::KEYBOARD.lock().read();
-
-      // If the user hits 'q', exit.
-      if scancode == 16 {
-        io::PICS.lock().initialize();
-        println!("Interrupts engaged");
-        panic!();
-      }
-
-      println!("Got keyboard code {}", scancode);
-    }
-  }
-  **/
-//}
 
 #[lang = "eh_personality"] extern fn eh_personality() {}
 
--- src/memory/paging/mod.rs
+++ src/memory/paging/mod.rs
@@ -1,15 +1,15 @@
-use memory::PAGE_SIZE; // needed later
+use memory::{PAGE_SIZE, Frame, FrameAllocator};
+use multiboot2::BootInformation;
 
 mod entry;
+mod mapper;
 mod table;
-
 mod temporary_page;
 
 pub use self::entry::*;
 use self::table::{Table, Level4};
+use self::temporary_page::TemporaryPage;
 use core::ptr::Unique;
-use memory::FrameAllocator;
-use memory::Frame;
 
 const ENTRY_COUNT: usize = 512;
 
@@ -47,13 +47,84 @@ impl Page {
   }
 }
 
+pub use self::mapper::Mapper;
+use core::ops::{Deref, DerefMut};
+
+pub struct ActivePageTable {
+    mapper: Mapper,
+}
+
+impl Deref for ActivePageTable {
+    type Target = Mapper;
+
+    fn deref(&self) -> &Mapper {
+        &self.mapper
+    }
+}
+
+impl DerefMut for ActivePageTable {
+    fn deref_mut(&mut self) -> &mut Mapper {
+        &mut self.mapper
+    }
+}
+
+impl ActivePageTable {
+  unsafe fn new() -> ActivePageTable {
+      ActivePageTable {
+          mapper: Mapper::new(),
+      }
+  }
+
+  pub fn with<F>(&mut self,
+                     table: &mut InactivePageTable,
+                     temporary_page: &mut temporary_page::TemporaryPage, // new
+                     f: F)
+      where F: FnOnce(&mut Mapper)
+  {
+      use x86::{controlregs, tlb};
+      let flush_tlb = || unsafe { tlb::flush_all() };
+
+      {
+          let backup = Frame::containing_address(
+              unsafe { controlregs::cr3() } as usize);
+
+          // map temporary_page to current p4 table
+          let p4_table = temporary_page.map_table_frame(backup.clone(), self);
+
+          // overwrite recursive mapping
+          self.p4_mut()[511].set(table.p4_frame.clone(), PRESENT | WRITABLE);
+          flush_tlb();
+
+          // execute f in the new context
+          f(self);
+
+          // restore recursive mapping to original p4 table
+          p4_table[511].set(backup, PRESENT | WRITABLE);
+          flush_tlb();
+      }
+
+      temporary_page.unmap(self);
+  }
+}
+
 pub struct InactivePageTable {
     p4_frame: Frame,
 }
 
 impl InactivePageTable {
-    pub fn new(frame: Frame) -> InactivePageTable {
-        // TODO zero and recursive map the frame
+    pub fn new(frame: Frame,
+               active_table: &mut ActivePageTable,
+               temporary_page: &mut TemporaryPage)
+               -> InactivePageTable {
+        {
+            let table = temporary_page.map_table_frame(frame.clone(), active_table);
+            // now we are able to zero the table
+            table.zero();
+            // set up recursive mapping for the table
+            table[511].set(frame.clone(), PRESENT | WRITABLE);
+        }
+        temporary_page.unmap(active_table);
+
         InactivePageTable { p4_frame: frame }
     }
 }
@@ -166,6 +237,52 @@ impl RecursivePageTable {
     allocator.deallocate_frame(frame);
   }
 
+}
+
+pub fn remap_the_kernel<A>(allocator: &mut A, boot_info: &BootInformation)
+    where A: FrameAllocator
+{
+    use core::ops::Range;
+
+    let mut temporary_page = TemporaryPage::new(Page { number: 0xcafebabe },
+        allocator);
+
+    let mut active_table = unsafe { ActivePageTable::new() };
+    let mut new_table = {
+        let frame = allocator.allocate_frame().expect("no more frames");
+        InactivePageTable::new(frame, &mut active_table, &mut temporary_page)
+    };
+
+    active_table.with(&mut new_table, &mut temporary_page, |mapper| {
+      let elf_sections_tag = boot_info.elf_sections_tag()
+          .expect("Memory map tag required");
+
+      for section in elf_sections_tag.sections() {
+        use multiboot2::ELF_SECTION_ALLOCATED;
+        use self::entry::WRITABLE;
+
+        if !section.flags().contains(ELF_SECTION_ALLOCATED) {
+            // section is not loaded to memory
+            continue;
+        }
+
+        println!("mapping section at addr: {:#x}, size: {:#x}",
+            section.addr, section.size);
+
+        let flags = WRITABLE; // TODO use real section flags
+
+        let range = Range {
+            start: section.addr as usize,
+            end: (section.addr + section.size) as usize,
+        };
+        for address in range.step_by(PAGE_SIZE) {
+            assert!(address % PAGE_SIZE == 0,
+                "sections need to be page aligned");
+            let frame = Frame::containing_address(address);
+            mapper.identity_map(frame, flags, allocator);
+        }
+      }
+    });
 }
 
 pub fn test_paging<A>(allocator: &mut A)
