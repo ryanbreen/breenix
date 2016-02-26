f3fd00a068bb8e5b1a37d204e31f697087d9b9fb
Ryan Breen <rbreen@cimpress.com>
2016-02-26 13:20:05 -0500

    Finish 64 bit conversion.

--- src/io/idt.rs
+++ src/io/idt.rs
@@ -6,7 +6,7 @@ struct IDT {
   idt_location: u64,
   idtr_location: u64,
   setup: bool,
-  data: [u8;0x800],
+  data: [u8;0x22FB],
   idtr: [u16;5],
 }
 
@@ -31,7 +31,7 @@ impl IDT {
           setup: false,
           idt_location: 0,
           idtr_location: 0,
-          data: [0; 0x800],
+          data: [0; 0x22FB],
           idtr: [0; 5]
       };
       idt.initialize();
@@ -60,7 +60,7 @@ impl IDT {
     println!("Test fn lives at 0x{:x}", fn_ptr);
 
     // Initialize idtr
-    self.idtr[0] = 0x800-1;
+    self.idtr[0] = 0x22FB-1;
     self.idtr[1] = ((self.idt_location & 0xffff000000000000) >> 48) as u16;
     self.idtr[2] = ((self.idt_location & 0xffff00000000) >> 32) as u16;
     self.idtr[3] = ((self.idt_location & 0xffff0000) >> 16) as u16;
@@ -71,7 +71,7 @@ impl IDT {
 
     unsafe {
       asm!("lidt %idtr" :: "{idtr}"(self.idtr_location) :: "volatile");
-      //asm!("int $$0x2f" :::: "volatile");
+      asm!("int $$0x2f" :::: "volatile");
     }
   }
 
@@ -80,12 +80,12 @@ impl IDT {
       panic!("Invalid IDT!");
     }
 
-    let i:usize = idx as usize * 8; // Each IDT entry is 8 bytes
+    let i:usize = idx as usize * 16; // Each IDT entry is 8 bytes
     //(uint16_t*)(idt_location + 8*i + 0) = (uint16_t)(callback & 0x0000ffff);
     self.data[i] =   ((callback & 0x0000ff00) >> 8) as u8;
     self.data[i+1] = (callback & 0x000000ff) as u8;
     //*(uint16_t*)(idt_location + 8*i + 2) = (uint16_t)0x8;
-    self.data[i+2] = 0x8 as u8;
+    self.data[i+3] = 0x8 as u8;
     //*(uint8_t*) (idt_location + 8*i + 4) = 0x00;
     self.data[i+4] = 0x00 as u8;
     //*(uint8_t*) (idt_location + 8*i + 5) = 0x8e;//0 | IDT_32BIT_INTERRUPT_GATE | IDT_PRESENT;
@@ -93,7 +93,14 @@ impl IDT {
     //*(uint16_t*)(idt_location + 8*i + 6) = (uint16_t)((callback & 0xffff0000) >> 16);
     self.data[i+6] = ((callback & 0xff000000) >> 24) as u8;
     self.data[i+7] = ((callback & 0x00ff0000) >> 16) as u8;
-    println!("{:x} {:x} {:x} {:x}", self.data[i+6], self.data[i+7], self.data[i], self.data[i+1]);
+    self.data[i+8] = ((callback & 0xff0000000000) >> 40) as u8;
+    self.data[i+9] = ((callback & 0x00ff00000000) >> 32) as u8;
+    self.data[i+10] = ((callback & 0xff00000000000000) >> 60) as u8;
+    self.data[i+11] = ((callback & 0x00ff000000000000) >> 52) as u8;
+    println!("{:x} {:x} {:x} {:x} {:x} {:x} {:x} {:x}",
+      self.data[i+10], self.data[i+11], self.data[i+8], self.data[i+9],
+      self.data[i+6], self.data[i+7], self.data[i], self.data[i+1]
+    );
     unsafe {
       if test_success {
         println!("Registered INT#{}", idx);
