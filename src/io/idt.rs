1487e0e07ddb0c022ecaeb6a96537bc4b7728c6b
Ryan Breen <rbreen@cimpress.com>
2016-02-27 12:52:59 -0500

    Wrestling with byte-alignment issues on the idt.

--- src/io/idt.rs
+++ src/io/idt.rs
@@ -1,5 +1,5 @@
 
-#[repr(C, packed)]
+#[repr(C)]
 #[derive(Copy, Clone, Debug)]
 struct IDTEntry {
   clbk_low: u16,
@@ -11,10 +11,10 @@ struct IDTEntry {
   zero2: u32
 }
 
-#[repr(C, packed)]
+#[repr(C)]
 struct IDTable {
   limit: u16,
-  base: *const [IDTEntry;IDT_SIZE]
+  base: u64
 }
 
 const IDT_SIZE: usize = 256;
@@ -23,11 +23,9 @@ static mut test_success:bool = false;
 static mut idt_init:bool = false;
 
 #[no_mangle]
-pub extern "C" fn idt_test_handler() {
-  unsafe {
-    println!("Test handler called");
-    test_success = true;
-  }
+pub unsafe extern "C" fn idt_test_handler() {
+  println!("Test handler called");
+  test_success = true;
 }
 
 #[no_mangle]
@@ -39,6 +37,7 @@ pub extern "C" fn idt_default_handler() {
 // All the entries are statically initialized so that all interrupts are by
 // default handled by a function that do nothing.
 // Specialized handlers will come later
+#[repr(C)]
 static mut descriptors: [IDTEntry;IDT_SIZE] = [IDTEntry {
     clbk_low:  0,
     clbk_mid:  0,
@@ -49,9 +48,10 @@ static mut descriptors: [IDTEntry;IDT_SIZE] = [IDTEntry {
     zero2: 0
 };IDT_SIZE];
 
+#[repr(C)]
 static mut idt_table: IDTable = IDTable {
   limit: 0, 
-  base: 0 as *const [IDTEntry;IDT_SIZE]
+  base: 0
 };
 
 pub unsafe fn load_descriptor(num: usize, clbk: u64, flags: u8, selector: u16) {
@@ -60,9 +60,10 @@ pub unsafe fn load_descriptor(num: usize, clbk: u64, flags: u8, selector: u16) {
     return;
   }
 
-  descriptors[num].clbk_low  = (clbk & 0xFFFF) as u16;
-  descriptors[num].clbk_mid  = ((clbk >> 16) & 0xFFFF) as u16;
-  descriptors[num].clbk_high = ((clbk >> 32) & 0xFFFFFFFF) as u32;
+  descriptors[num].clbk_low  = ((clbk as u64) & 0xFFFF) as u16;
+  descriptors[num].clbk_mid  = (((clbk as u64) >> 16) & 0xFFFF) as u16;
+  descriptors[num].clbk_high = (((clbk as u64) >> 32) & 0xFFFFFFFF) as u32;
+  println!("{:x} {:x} {:x}", descriptors[num].clbk_high, descriptors[num].clbk_mid, descriptors[num].clbk_low);
   descriptors[num].selector = selector;
   descriptors[num].flags = flags;
 }
@@ -78,31 +79,42 @@ pub fn setup() {
 
     idt_init = false;
 
-    // FIXME: this souldn't be necessary (see above)
-    idt_table.limit = (IDT_SIZE as u16) * 8;
-    idt_table.base = &descriptors as *const [IDTEntry;256];
-
     // FIXME: this shouldn't be necessary (see above)
-    let mut i = 0;
+    idt_table.limit = ((IDT_SIZE as u16) * 128) - 1;
+    idt_table.base = &descriptors as *const _ as u64;
+
     let clbk_addr = &idt_default_handler as *const _ as u64;
-    while i < IDT_SIZE {
+    for i in 0..IDT_SIZE {
       load_descriptor(i, clbk_addr, 0x8E, 0x08);
-      i += 1
     }
 
     let fn_ptr = &idt_test_handler as *const _ as u64;
     load_descriptor(0x2f, fn_ptr, 0x8E, 0x08);
+    println!("Initted test handler {:x}", fn_ptr);
 
-    let idt_table_address = (&idt_table as * const _ as u64);
+
+    let idt_table_address = idt_table.base;
+    let entry_at_offset = idt_table_address + (0x80 * 0x10);
+    println!("idt starts at {:x}, entry at {:x}, delta {:x}", idt_table_address, entry_at_offset, entry_at_offset - idt_table_address);
+    
+    let idt_entry = *(entry_at_offset as *const IDTEntry);
+    println!("{:?}", idt_entry);
+    println!("{:x}", (idt_entry.clbk_high as u64) << 32 | (idt_entry.clbk_mid as u64) << 16 | idt_entry.clbk_low as u64);
+
+/*
+    let idt_table_address = idt_table.base;
     let entry_at_offset = idt_table_address + (0x2F*0x80);
-    println!("idt starts at {}, entry at {}, delta {}", idt_table_address, entry_at_offset, entry_at_offset - idt_table_address);
-    //println!("{:?}", *(entry_at_offset as *const IDTEntry));
-
-    println!("Initted test handler {}", fn_ptr);
-
-    asm!("lidt ($0)" :: "r" (idt_table_address));
+    println!("idt starts at {:x}, entry at {:x}, delta {:x}", idt_table_address, entry_at_offset, entry_at_offset - idt_table_address);
+    
+    let idt_entry = *(entry_at_offset as *const IDTEntry);
+    println!("{:?}", idt_entry);
+    println!("{:x}", (idt_entry.clbk_high as u64) << 32 | (idt_entry.clbk_mid as u64) << 16 | idt_entry.clbk_low as u64);
+*/
+    asm!("lidt ($0)" :: "r" (&idt_table as *const _ as u64));
     //asm!("sti");
-    asm!("int $$0x2f" :::: "volatile");
+    //asm!("int $$0x2f" :::: "volatile");
+    asm!("int $$0x12" :::: "volatile");
+    
   }
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -72,8 +72,7 @@ pub extern "C" fn rust_main(multiboot_information_address: usize) {
     enable_nxe_bit();
     enable_write_protect_bit();
     memory::remap_the_kernel(&mut frame_allocator, boot_info);
-    println!("It did not crash!");
-
+    
     io::idt::setup();
     io::keyboard::test();
 }
