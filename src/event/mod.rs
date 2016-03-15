6719590f5449a2aa0994a104ae308df720749c46
Ryan Breen <rbreen@cimpress.com>
2016-03-15 14:30:31 -0400

    Only dispatch key events if listeners if a valid object.

--- src/event/mod.rs
+++ src/event/mod.rs
@@ -24,9 +24,6 @@ pub trait IsListener<T: IsEvent> {
   fn notify(&self, ev: &T);
 }
 
-//pub static mut KEY_EVENT_LISTENERS:Mutex<Box<Option<Vec<Box<IsListener<KeyEvent>>>>>> = Mutex::new(Box::new(None));
-//pub static mut KEY_EVENT_LISTENERS:Mutex<Option<Vec<Box<IsListener<KeyEvent>>>>> = Mutex::new(None);
-
 static mut KEY_EVENT_LISTENERS_PTR:usize = 0;
 
 pub fn key_event_listeners() -> Option<&'static mut Vec<Box<IsListener<KeyEvent>>>> {
@@ -48,10 +45,11 @@ pub fn set_key_event_listener(listeners: &'static mut Vec<Box<IsListener<KeyEven
 }
 
 pub fn dispatch_key_event(ev: &KeyEvent) {
-  let listeners = key_event_listeners().unwrap();
-  for listener in listeners {
-    if listener.handles_event(ev) {
-      listener.notify(ev);
+  if let Some(listeners) = key_event_listeners() {
+    for listener in listeners {
+      if listener.handles_event(ev) {
+        listener.notify(ev);
+      }
     }
   }
 }
