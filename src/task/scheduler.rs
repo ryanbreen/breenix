4a1199041b7ff1dd87aa5a45d4ade974f45e8dbd
Ryan Breen <rbreen@cimpress.com>
2016-04-08 20:37:07 -0400

    Start stubbing out scheduler.

--- src/task/scheduler.rs
+++ src/task/scheduler.rs
@@ -1,4 +1,14 @@
 
-struct Scheduler {
+pub struct Scheduler {
   tasks: Box<Vec<Task>>,
+}
+
+impl Scheduler {
+  pub fn add_task(&mut self, task: &Task) {
+    self.tasks.push(Box::new(task));
+  }
+
+  pub fn schedule(&mut self) {
+    
+  }
 }
