21fef99f560de26a2598bc97445a3865675cabf9
Ryan Breen <rbreen@cimpress.com>
2016-04-11 20:50:45 -0400

    Switch to an Option for tracking state.

--- src/state.rs
+++ src/state.rs
@@ -1,31 +1,52 @@
 
 use alloc::boxed::Box;
 use collections::vec::Vec;
-use core::ptr::Unique;
 
 use event::IsListener;
 use event::keyboard::KeyEvent;
 
-struct State {
-  key_listeners: Vec<Box<IsListener<KeyEvent>>>,
+pub struct State {
+  pub key_listeners: Vec<Box<IsListener<KeyEvent>>>,
 }
 
-static mut STATE_PTR:Unique<State> = unsafe { Unique::new(0xfff000 as * mut _) };
+impl State {
+  fn new() -> Box<State> {
+    box State {
+      key_listeners: Vec::new()
+    }
+  }
+}
 
-fn key_event_listeners() -> &'static mut Vec<Box<IsListener<KeyEvent>>> {
+static mut STATE_PTR: Option<&'static mut State> = None;
+
+pub fn state() -> &'static mut State {
   unsafe {
-    return &mut(STATE_PTR.get_mut().key_listeners);
+    match STATE_PTR {
+      Some(ref mut p) => p,
+      None => {
+        STATE_PTR = Some(&mut *Box::into_raw(State::new()));
+        match STATE_PTR {
+          Some(ref mut s) => {
+            /// Do any sort of state init we need beyond new().
+            s
+          },
+          None => {
+            panic!("Failed to init state");
+          }
+        }
+      },
+    }
   }
 }
 
 pub fn register_key_event_listener(listener: Box<IsListener<KeyEvent>>) {
-  let mut listeners:&'static mut Vec<Box<IsListener<KeyEvent>>> = key_event_listeners();
-  listeners.push(listener);
-  println!("There are now {} key listeners", listeners.len());
+  state().key_listeners.push(listener);
+  println!("There are now {} key listeners", state().key_listeners.len());
 }
 
 pub fn dispatch_key_event(ev: &KeyEvent) {
-  for listener in key_event_listeners() {
+  let listeners = &(state().key_listeners);
+  for listener in listeners {
     if listener.handles_event(ev) {
       listener.notify(ev);
     }
