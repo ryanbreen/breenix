d042f934b8dd0815e467f590b5db53e327d312a7
Ryan Breen <rbreen@cimpress.com>
2016-04-23 22:58:17 -0400

    Add frame data to debug output.

--- src/buffers.rs
+++ src/buffers.rs
@@ -137,6 +137,8 @@ pub fn debug() {
   use x86::msr::rdmsr;
   use x86::time::rdtsc;
 
+  use memory;
+
   let mut buffer = DEBUG_BUFFER.lock();
   unsafe {
     let time = timer::time_since_start();
@@ -148,8 +150,8 @@ pub fn debug() {
     buffer.write_fmt(format_args!("msr MSR_MCG_RFLAGS: 0x{:x}\n", rdmsr(MSR_MCG_RFLAGS)));
     buffer.write_fmt(format_args!("interrupt count: 0x20={}, 0x21={}, 0x80={}\n",
       ::state().interrupt_count[0x20], ::state().interrupt_count[0x21], ::state().interrupt_count[0x80]));
-    buffer.write_fmt(format_args!("allocated frame count: 0x20={}, 0x21={}, 0x80={}\n",
-      ::state().interrupt_count[0x20], ::state().interrupt_count[0x21], ::state().interrupt_count[0x80]));
+    buffer.write_fmt(format_args!("allocated frame count: 0={}\n",
+      memory::frame_allocator().allocated_frame_count()));
   }
 }
 
