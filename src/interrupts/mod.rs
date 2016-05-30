9e3f914883bbeffdcf3e2c86727fbbc70f54177b
Ryan Breen <rbreen@cimpress.com>
2016-05-30 11:43:29 -0700

    Start migrating over to PhilOpp interrupts.

--- src/interrupts/mod.rs
+++ src/interrupts/mod.rs
@@ -29,3 +29,22 @@ pub fn init() {
 /// Interface to our PIC (programmable interrupt controller) chips.  We
 /// want to map hardware interrupts to 0x20 (for PIC1) or 0x28 (for PIC2).
 pub static PICS: Mutex<ChainedPics> = Mutex::new(unsafe { ChainedPics::new(0x20, 0x28) });
+
+/// Various data available on our stack when handling an interrupt.
+#[repr(C, packed)]
+#[derive(Debug)]
+struct InterruptContext {
+    rsi: u64,
+    rdi: u64,
+    r11: u64,
+    r10: u64,
+    r9: u64,
+    r8: u64,
+    rdx: u64,
+    rcx: u64,
+    rax: u64,
+    int_id: u32,
+    _pad_1: u32,
+    error_code: u32,
+    _pad_2: u32,
+}
