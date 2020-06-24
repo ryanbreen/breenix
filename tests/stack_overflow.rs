8b08f7c672ed66d5d8231e4df1c0dc7863971de7
Ryan Breen <ryan@ryanbreen.com>
2020-06-23 22:19:53 -0400

    Add a stack overflow integration test.

---
+++ tests/stack_overflow.rs
@@ -0,0 +1,63 @@
+#![no_std]
+#![no_main]
+
+#![feature(abi_x86_interrupt)]
+
+use core::panic::PanicInfo;
+
+use breenix::{serial_print, serial_println};
+use breenix::{exit_qemu, QemuExitCode};
+use x86_64::structures::idt::InterruptStackFrame;
+
+use lazy_static::lazy_static;
+use x86_64::structures::idt::InterruptDescriptorTable;
+
+lazy_static! {
+    static ref TEST_IDT: InterruptDescriptorTable = {
+        let mut idt = InterruptDescriptorTable::new();
+        unsafe {
+            idt.double_fault
+                .set_handler_fn(test_double_fault_handler)
+                .set_stack_index(breenix::constants::interrupts::DOUBLE_FAULT_IST_INDEX as u16);
+        }
+
+        idt
+    };
+}
+
+pub fn init_test_idt() {
+    TEST_IDT.load();
+}
+
+extern "x86-interrupt" fn test_double_fault_handler(
+    _stack_frame: &mut InterruptStackFrame,
+    _error_code: u64,
+) -> ! {
+    serial_println!("[ok]");
+    exit_qemu(QemuExitCode::Success);
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    serial_print!("stack_overflow::stack_overflow...\t");
+
+    breenix::interrupts::gdt::init();
+    init_test_idt();
+
+    // trigger a stack overflow
+    stack_overflow();
+
+    panic!("Execution continued after stack overflow");
+}
+
+#[allow(unconditional_recursion)]
+fn stack_overflow() {
+    stack_overflow(); // for each recursion, the return address is pushed
+    volatile::Volatile::new(0).read(); // prevent tail recursion optimizations
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    breenix::test_panic_handler(info)
+}
